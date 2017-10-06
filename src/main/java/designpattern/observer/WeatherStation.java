a9aa4960667cf787ab0c06ecbc8f9db606bae1f7
Maxim Wiens <maxim.wiens@itsd-consulting.de>
2017-10-06 12:43:19 +0200

    Forecast

--- src/main/java/designpattern/observer/WeatherStation.java
+++ src/main/java/designpattern/observer/WeatherStation.java
@@ -1,5 +1,6 @@
 package designpattern.observer;
 
+import designpattern.observer.classes.Forecast;
 import designpattern.observer.classes.Statistics;
 import designpattern.observer.classes.Subscriber;
 import designpattern.observer.classes.WeatherData;
@@ -9,8 +10,10 @@ public class WeatherStation {
         WeatherData weatherData = new WeatherData();
         Subscriber subscriber = new Subscriber(weatherData);
         Statistics statistics = new Statistics(weatherData);
+        Forecast forecast = new Forecast(weatherData);
         weatherData.setMesswerte(30, 65,30.4f);
         weatherData.setMesswerte(32, 70,29.2f);
+        weatherData.setMesswerte(28, 90, 30.4f);
         weatherData.setMesswerte(28, 90, 30.4f);
     }
 }
--- src/main/java/designpattern/observer/classes/Forecast.java
+++ src/main/java/designpattern/observer/classes/Forecast.java
@@ -3,15 +3,37 @@ package designpattern.observer.classes;
 import designpattern.observer.interfaces.Observer;
 import designpattern.observer.interfaces.ShowElement;
 
+import java.util.logging.Logger;
+
 public class Forecast implements Observer, ShowElement {
+
+    private float presCurr = 29.9f;
+    private float presLast;
+    private WeatherData weatherData;
+    private Logger logger = Logger.getLogger(Statistics.class.getName());
+
+    public Forecast(WeatherData weatherData){
+        this.weatherData = weatherData;
+        this.weatherData.registerObserver(this);
+    }
 
     @Override
     public void refresh(float temp, float hum, float press) {
-
+        this.presLast=this.presCurr;
+        this.presCurr = press;
+        show();
     }
 
     @Override
     public void show() {
+    if(this.presCurr<presLast){
+        logger.info("Vorhersage: Gehen Sie von kälterem, regnerischem Wetter aus.");
+
+    }  else if (this.presLast==this.presCurr){
+        logger.info("Vorhersage: Wetter geht weiter so.");
+    }else{
+        logger.info("Vorhersage: Wetterbesserung in Sicht.");
+    }
 
     }
 }
